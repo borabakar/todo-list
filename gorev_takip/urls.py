bacc9c357193956f6457060f4f0a09d11c376742
borabakar <bora.bakar.29@gmail.com>
2025-05-25 23:21:25 +0300

    Update urls.py

--- gorev_takip/urls.py
+++ gorev_takip/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),  # ← BU ŞART
+    path('', include('tasks.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # login/logout
 ]
